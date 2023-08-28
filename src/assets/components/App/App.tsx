e5ded0549070a87d821d5b57ab334f5d743ee999
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-08-28 15:50:16 +0800

    feat: create Theme context and app background colors

--- src/assets/components/App/App.tsx
+++ src/assets/components/App/App.tsx
@@ -1,11 +1,30 @@
 import "./App.scss";
 
+import { useState, createContext } from "react";
+
+enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+type ThemeContextType = {
+  theme: Theme;
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+};
+
+const ThemeContext = createContext<null | ThemeContextType>(null);
+
 const App = () => {
+  const [theme, setTheme] = useState(Theme.LIGHT);
+
   return (
-    <main className="app">
-      <div className="app-bg"></div>
-      <div className="app-bg app-bg__top"></div>
-    </main>
+    <div className={`app app-bg--${theme}`}>
+      <div className={`app-bg__top app-bg__top--${theme}`}></div>
+
+      <ThemeContext.Provider value={{ theme: theme, setTheme: setTheme }}>
+        <main></main>
+      </ThemeContext.Provider>
+    </div>
   );
 };
 
