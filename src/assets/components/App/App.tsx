e2e0a6846a94d21540e3869ea125c8c5eadca19b
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-08-28 17:30:03 +0800

    feat: implement theme toggle of header section

--- src/assets/components/App/App.tsx
+++ src/assets/components/App/App.tsx
@@ -17,12 +17,43 @@ const ThemeContext = createContext<null | ThemeContextType>(null);
 const App = () => {
   const [theme, setTheme] = useState(Theme.LIGHT);
 
+  function toggleTheme() {
+    switch (theme) {
+      case Theme.LIGHT:
+        setTheme(Theme.DARK);
+        break;
+
+      case Theme.DARK:
+        setTheme(Theme.LIGHT);
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div className={`app app-bg--${theme}`}>
       <div className={`app-bg__top app-bg__top--${theme}`}></div>
 
       <ThemeContext.Provider value={{ theme: theme, setTheme: setTheme }}>
-        <main></main>
+        <main className="dashboard-content">
+          <div className={`header-text header-text--${theme} vcsb`}>
+            <div>
+              <h1>Social Media Dashboard</h1>
+              <span>Total Followers: 23,004</span>
+            </div>
+            <div className="theme-toggle vc">
+              <label>Dark Mode</label>
+              <input
+                title="theme-toggle"
+                type="checkbox"
+                checked={theme === Theme.DARK}
+                onClick={toggleTheme}
+              ></input>
+            </div>
+          </div>
+        </main>
       </ThemeContext.Provider>
     </div>
   );
