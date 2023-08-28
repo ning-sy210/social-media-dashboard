898e3a4161e368f514c97a6332c8784895914d8d
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-08-28 14:54:51 +0800

    feat: create App component

---
+++ src/assets/components/App/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+const App = () => {
+  return (
+    <main className="app">
+      <div className="app-bg"></div>
+      <div className="app-bg app-bg__top"></div>
+    </main>
+  );
+};
+
+export default App;
