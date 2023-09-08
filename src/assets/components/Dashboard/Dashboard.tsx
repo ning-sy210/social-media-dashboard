4d10073b90871c6983b284a246d9a376dac8b89e
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-09-08 10:59:13 +0800

    feat: implement auto-grid for card container

--- src/assets/components/Dashboard/Dashboard.tsx
+++ src/assets/components/Dashboard/Dashboard.tsx
@@ -9,34 +9,36 @@ const Dashboard = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <main className="dashboard-content stack">
-      <div className={`header-text header-text--${theme} vcsb`}>
-        <div>
-          <h1>Social Media Dashboard</h1>
-          <span>Total Followers: 23,004</span>
+    <div className="dashboard-content-ctn stack-hc">
+      <main className="dashboard-content stack">
+        <div className={`header-text header-text--${theme} vcsb`}>
+          <div>
+            <h1 className="h2">Social Media Dashboard</h1>
+            <span className="h4">Total Followers: 23,004</span>
+          </div>
+          <div className="theme-toggle vc">
+            <label className="h4">Dark Mode</label>
+            <input
+              title="Toggle theme"
+              type="checkbox"
+              checked={theme === Theme.DARK}
+              onClick={toggleTheme}
+            ></input>
+          </div>
         </div>
-        <div className="theme-toggle vc">
-          <label>Dark Mode</label>
-          <input
-            title="Toggle theme"
-            type="checkbox"
-            checked={theme === Theme.DARK}
-            onClick={toggleTheme}
-          ></input>
-        </div>
-      </div>
 
-      <div className="flex-sb">
-        {Object.entries(socialMediaData).map(([platform, accountInfo]) => (
-          <DashboardSocialMainCard
-            platform={platform}
-            username={accountInfo.username}
-            followerCount={accountInfo.followers.count}
-            followerCountChange={accountInfo.followers.countChange}
-          />
-        ))}
-      </div>
-    </main>
+        <div className="auto-grid" style={{ gap: "16px 32px" }}>
+          {Object.entries(socialMediaData).map(([platform, accountInfo]) => (
+            <DashboardSocialMainCard
+              platform={platform}
+              username={accountInfo.username}
+              followerCount={accountInfo.followers.count}
+              followerCountChange={accountInfo.followers.countChange}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   );
 };
 
